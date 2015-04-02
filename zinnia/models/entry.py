57b6511bb7f41dab5d1190f0b8ca663f221edf03
Jon Simington <jon.simington@mst.edu>
2015-04-02 21:02:42 +0000

    Change ratings app

--- zinnia/models/entry.py
+++ zinnia/models/entry.py
@@ -2,11 +2,11 @@
 from zinnia.settings import ENTRY_BASE_MODEL
 from zinnia.models_bases import load_model_class
 
-from djangoratings.fields import RatingField
+from ratings.handlers import ratings
 
 class Entry(load_model_class(ENTRY_BASE_MODEL)):
     """
     The final Entry model based on inheritence.
     """
-    
-    rating = RatingField(range=5) # 5 possible rating values, 1-5
+
+ratings.register(Entry)
