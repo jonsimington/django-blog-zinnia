980d260b855d89064f2c7c0142d98da2ff7597c5
Jon Simington <jon.simington@mst.edu>
2015-04-02 20:19:41 +0000

    Import ratings

--- zinnia/models/entry.py
+++ zinnia/models/entry.py
@@ -2,6 +2,7 @@
 from zinnia.settings import ENTRY_BASE_MODEL
 from zinnia.models_bases import load_model_class
 
+from djangoratings.fields import RatingField
 
 class Entry(load_model_class(ENTRY_BASE_MODEL)):
     """
