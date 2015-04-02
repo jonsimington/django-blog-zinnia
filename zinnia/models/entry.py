8fdb05bbbb774d454175fb46366376d1d370f048
Jon Simington <jon.simington@mst.edu>
2015-04-02 20:11:31 +0000

    add ratings to entries

--- zinnia/models/entry.py
+++ zinnia/models/entry.py
@@ -7,3 +7,5 @@ class Entry(load_model_class(ENTRY_BASE_MODEL)):
     """
     The final Entry model based on inheritence.
     """
+    
+    rating = RatingField(range=5) # 5 possible rating values, 1-5
